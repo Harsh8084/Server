422e2f62d57611069978736c587baeee177ddff0
unknown <harshagarwal9771@gmail.com>
2025-04-19 13:56:04 +0530

    first commit

---
+++ CodeExecuter/testcases/lca0.js
@@ -0,0 +1,17 @@
+const input = [
+    "3 5 1 6 2 0 8 null null 7 4, 5, 1",
+    "3 5 1 6 2 0 8 null null 7 4, 5, 4",
+    "3 5 1 6 2 0 8 null null 7 4, 5, 6",
+    "3 5 1 6 2 0 8 null null 7 4, 7, 4",
+    "3 5 1 6 2 0 8 null null 7 4, 6, 4"
+];
+
+const output = [
+    "3",
+    "5",
+    "5",
+    "7",
+    "2"
+];
+
+module.exports = { input, output };
---
+++ CodeExecuter/testcases/substring0.js
@@ -0,0 +1,19 @@
+const input = [
+    "\"abcabcbb\"",
+    "\"bbbbb\"",
+    "\"pwwkew\"",
+    "\"\"",
+    "\"dvdf\"",
+    "\"aab\""
+];
+
+const output = [
+    "3",
+    "1",
+    "3",
+    "0",
+    "3",
+    "2"
+];
+
+module.exports = { input, output };
